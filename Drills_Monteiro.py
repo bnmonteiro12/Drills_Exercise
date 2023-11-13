dc1c58425867804d36880553829dd10a0586489e
bnmonteiro12 <bnmonteiro12@up.edu.ph>
2023-11-13 12:39:07 +0800

    Added File

--- Drills_Monteiro.py
+++ Drills_Monteiro.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import random as random
 df = pd.read_csv("table1.csv")
-#df.insert(0, "id", range(1,1+len(df)))
-#df.iloc[:,1] = 'META-' + df.iloc[:,1].astype(str)
-#df.insert(2, "Random", [random.randint(0,1) for i in range(0,len(df))])
+df.insert(0, "id", range(1,1+len(df)))
+df.iloc[:,1] = 'META-' + df.iloc[:,1].astype(str)
+df.insert(2, "Random", [random.randint(0,1) for i in range(0,len(df))])
 tdf = df.T
 print(tdf.head())
 print(df)
